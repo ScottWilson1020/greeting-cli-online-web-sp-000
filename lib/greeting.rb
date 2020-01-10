29e814dfa622be12b37dc9de018e40fd611c050d
Scott Wilson <scottwilson1020@gmail.com>
2020-01-10 23:52:37 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,6 +2,10 @@
 
 def greeting(name)
   
+<<<<<<< HEAD
  puts "Hello #{name.strip}. It's nice to meet you."
+=======
+ puts "Hello #{name}. It's nice to meet you."
+>>>>>>> 5dd45d6d31c25e7df48fe1b1ba6379d3e28a2b01
   
 end
